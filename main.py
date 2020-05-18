1d8cb7add96e40c722f1643bf7a4166042d4290b
bobreddy2009 <bobreddy2009@yahoo.com>
2020-05-18 04:32:06 +0000

    class 2

--- main.py
+++ main.py
@@ -1,2 +1,60 @@
+# day 1
+print ("DAY 1")
+
 print ("45 * 45 = " + str(45 * 45))
+
 print ("it")
+# day 2 
+print ("-----" * 3)
+
+print ("DAY 2")
+
+a = int(input("enter a number "))
+b = int (input("enter a number"))
+print (f"the sum of {a} + {b} = {a + b}")
+
+
+print ("---" * 3)
+first_name=input('Enter your firstname')
+last_name=input('Enter your last name')
+address=input('Enter your address')
+zipcode=input('Enter your zipcode')
+birth_year= int (input('Enter your year of birth'))
+hi = 2020
+age = hi - birth_year
+print(f'''Name: {first_name} {last_name}
+Address: {address}
+Zip: {zipcode}
+Age: {age}''')
+print ("---" * 3)
+number1 = int(input("Enter number1 "))
+number2 = int(input("Enter number2 "))
+
+print(f'Result of {number1} + {number2} = {number1 + number2}')
+print(f'Result of {number1} - {number2} = {number1 - number2}')
+print(f'Result of {number1} * {number2} = {number1 * number2}')
+print(f'Result of {number1} / {number2} = {number1 / number2}')
+
+# % means modulus (Reminder)
+print(f'Result of {number1} % {number2} = {number1 % number2}')
+
+# ** means Exponent
+print(f'Result of {number1} ** {number2} = {number1 ** number2}')
+
+# // means integer division
+print(f'Result of {number1} // {number2} = {number1 // number2}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
