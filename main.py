b21c238e66c30ab9c29b5f4d876fc67c73d7a3e4
null null <bobreddy2009@yahoo.com>
2020-05-25 00:00:42 +0000

    homework day 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # day 1
-print ("DAY 1")
+
 
 print ("45 * 45 = " + str(45 * 45))
 
@@ -7,7 +7,6 @@ print ("it")
 # day 2 
 print ("-----" * 3)
 
-print ("DAY 2")
 
 a = int(input("enter a number "))
 b = int (input("enter a number"))
@@ -15,6 +14,7 @@ print (f"the sum of {a} + {b} = {a + b}")
 
 
 print ("---" * 3)
+#day2 project
 first_name=input('Enter your firstname')
 last_name=input('Enter your last name')
 address=input('Enter your address')
@@ -27,6 +27,7 @@ Address: {address}
 Zip: {zipcode}
 Age: {age}''')
 print ("---" * 3)
+#python operations
 number1 = int(input("Enter number1 "))
 number2 = int(input("Enter number2 "))
 
@@ -43,9 +44,25 @@ print(f'Result of {number1} ** {number2} = {number1 ** number2}')
 
 # // means integer division
 print(f'Result of {number1} // {number2} = {number1 // number2}')
-
-
-
+print ("---" * 3)
+#string methods (homework day2)
+string = "My NaMe IS bob"
+x = string.swapcase()
+print (x)
+string = "HELLO, AND WELCOME TO MY WORLD!"
+x = string.casefold()
+print(x)
+string = "I like mangoes"
+x = string.replace("mangoes", "fruit")
+print(x)
+string = "bob"
+x = string.center(30)
+print(x)
+string = "Hello, welcome"
+x = string.endswith("come")
+print(x)
+print ("---" * 3)
+#day3
 
 
 
